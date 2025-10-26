99e598c8020a9489bc431a1f883001f1053083ec
moha623 <chaabanemed84@gmail.com>
2025-10-26 12:56:06 +0100

    vercel-3

--- src/app/technology/technology.ts
+++ src/app/technology/technology.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ScrollAnimationDirective } from '../scroll-animation.directive';
+
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-technology',
   standalone: true,
@@ -9,7 +12,15 @@ import { ScrollAnimationDirective } from '../scroll-animation.directive';
   styleUrls: ['./technology.css'],
 })
 export class Technology {
-  ngOnInit() {
-    window.scrollTo(0, 0);
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      window.scrollTo(0, 0);
+    }
   }
 }
