90b6857f95f14124522ff3047b0fad516e0af734
moha623 <chaabanemed84@gmail.com>
2025-10-27 13:48:33 +0100

    final-deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
  
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(), provideHotToastConfig(),
  
     
   ],
